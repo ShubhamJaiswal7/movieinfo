bb4dae4c1347721285e5e3207f48433e4a922144
ShubhamJiaswal7 <you@example.com>
2023-02-26 22:23:46 +0530

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Movie Info</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This is the IMDb clone website" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
